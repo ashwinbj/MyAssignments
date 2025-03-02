3479bd13901372406713a7b0f8e5d0afced24540
ashwinbj <63704665+ashwinbj@users.noreply.github.com>
2025-03-02 10:50:58 +0530

    Add files via upload
    Day1 Assignments

---
+++ day1/chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class chrome {
+public static void main(String[] args) {
+	 double version=91.0;
+	 String developer="Google";
+	 boolean isBeta=false;
+	 int releaseYear=2008;
+	 char shortcutKey='C';
+	 
+	 System.out.println("The New Version is:"+version);
+	 System.out.println("The Version Developed by:"+developer);
+	 System.out.println("The version is beta:"+isBeta);
+	 System.out.println("The Version Release year is:"+releaseYear);
+	 System.out.println("The ShortCut Key is:"+shortcutKey);
+
+
+}
+}
